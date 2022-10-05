f48f4fce36540e5b247abbb82b3259420329793c
Yokkin <yokkin.h@gmail.com>
2022-10-05 22:22:15 +0900

    deleted useless spaces

--- script.php
+++ script.php
@@ -1,16 +1,22 @@
 <?php
 class threemoji
 {
-	private const CHAR_DEF = 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよをんゔがぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽぁぃぅぇぉっゃゅょゎ';
-	private const BAD_CHAR_DEF = '/^(を|ん|ぁ|ぃ|ぅ|ぇ|ぉ|っ|ゃ|ゅ|ょ|ゎ)/';
+	private const CHAR_DEF = array (
+		'DICT' => 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよわんがぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽぁぃぅぇぉっゃゅょ',
+		'SCREEN' => array (
+			'/^[をんぁぃぅぇぉっゃゅょ].*$/u',
+			'/^(?!.*[きしちにひみりぎじびぴ][ゃゅょ]).*$/um',
+			'/^.[ぁぃぅぇぉゃゅょ]+$/um',
+	   	),
+	);
 	
 	public function __construct()
 	{
-		if (!empty(self::CHAR_DEF))
+		if (!empty(self::CHAR_DEF['DICT']))
 		{
 			// splits given characters with string by bytes specified and then contains as array items.
 			// note that Japanese characters are splited by 3 bytes in UTF-8.
-			$this->CHAR_ARRAY = str_split(self::CHAR_DEF, 3);
+			$this->CHAR_ARRAY = str_split(self::CHAR_DEF['DICT'], 3);
 			$this->CHAR_COUNT = 	count($this->CHAR_ARRAY);
 			
 			#echo var_dump(CHAR_ARRAY);
@@ -20,7 +26,31 @@ class threemoji
 		}
 	}
 	
-	public function generate_word(array $a, int $b): ?string 
+	public function generate_word_test(): ?string 
+	{
+		$word = 'みかる';
+		echo var_dump($word);
+		echo var_dump(self::CHAR_DEF);
+
+		if (
+			(preg_match(self::CHAR_DEF['SCREEN'][0], $word))
+		   	or 
+			(preg_match(self::CHAR_DEF['SCREEN'][1], $word))
+		)
+		{
+			return 'bad';
+		}
+		else
+		{
+			return 'ok'; 
+		}
+	}
+
+	// ====================================================================
+	//        IF YOU ARE NOW EDITING THIS FOR TEST NEVER TOUCH THIS
+	// ====================================================================
+	
+	public function generate_word( array $a, int $b,): ?string 
 	{
 		$i = 0; 
 		#echo var_dump($i);
@@ -30,9 +60,15 @@ class threemoji
 			$out_buffer_chunk[] = $a[rand(0, $b - 1)];	// 配列の中から1つえらんでバッファに格納
 			if(count($out_buffer_chunk)>= 3)	 // 配列のキーの総数が一定以上に達したら文字列に結合
 			{
-				$words_marged = implode($out_buffer_chunk);		
+				$words_merged = implode($out_buffer_chunk);		
 				
-				if(preg_match(self::BAD_CHAR_DEF, $words_marged))
+				if (
+					(preg_match(self::CHAR_DEF['SCREEN'][0], $words_merged))
+					or 
+					(preg_match(self::CHAR_DEF['SCREEN'][1], $words_merged))
+					or 
+					(preg_match(self::CHAR_DEF['SCREEN'][2], $words_merged))
+				)
 				{
 					#echo 'Retrying...';
 					return $this->generate_word($a, $b);
@@ -40,7 +76,8 @@ class threemoji
 				}
 				else
 				{
-					return $words_marged . PHP_EOL;
+					$result = $words_merged . PHP_EOL;
+					return $result; 
 					break; 
 				}
 			}
@@ -78,4 +115,14 @@ class threemoji
 }
 
 $instance = new threemoji; 
+
+// these are flags; uncomment these as you need
+
+// if you would like to post generated words
 $instance->post();
+
+// if you would like to test if the regex are valid 
+#echo $instance->generate_word_test();
+
+// if you would like to output the generated word into stdout 
+#echo $instance->generate_word($instance->CHAR_ARRAY, $instance->CHAR_COUNT);
